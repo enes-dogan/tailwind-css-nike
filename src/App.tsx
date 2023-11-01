21ad9c4315cb704d5f70a42ab77eb06102a5a258
enes-dogan <enesdogan210@gmail.com>
2023-11-01 17:53:45 +0300

    tailwind and react app initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-bold underline text-green-500 m-96">App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
