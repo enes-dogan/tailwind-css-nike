9de78e01450c72b2e34db0509adb6484b799a15d
enes-dogan <enesdogan210@gmail.com>
2023-11-01 19:24:16 +0300

    creating individual components for every section

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+} from './sections';
+
+import Nav from './components/Nav';
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffers
+      <SpecialOffers />
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
-    <section className="padding-x py-10">
-      Footer
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
     </section>
   </main>
-)
+);
 
 export default App;
