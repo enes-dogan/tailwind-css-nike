a86d929b8c1efe54734413140377b323cca1595f
enes-dogan <enesdogan210@gmail.com>
2023-11-01 19:07:06 +0300

    ditching typescript because of eslint/prettier collision

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <div className="flex justify-center">
-        <h1 className="text-3xl font-bold underline text-green-500 m-96">App</h1>
-      </div>
-    </>
-  );
-}
